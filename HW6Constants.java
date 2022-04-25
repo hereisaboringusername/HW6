b98ecf1bb0e307301acf4b87022a6c1b626f3ea4
hereisaboringusername <71794563+hereisaboringusername@users.noreply.github.com>
2022-04-25 11:37:31 -0400

    Create HW6Constants.java

---
+++ HW6Constants.java
@@ -0,0 +1,23 @@
+package edu.monmouth.hw6;
+
+public class HW6Constants {
+/**
+         * Name of file to redirect standard err and out
+         */
+        public static final String OUT_FILE_NAME = "HW6.txt";
+        
+        /**
+         * Name of file containing data to create String objects
+         */
+        public static final String FILENAME1 = "String.txt";
+        
+        /**
+         * Name of file containing data to create Book objects
+         */
+        public static final String FILENAME2 = "Book.txt";
+        
+        /**
+         * Separator of data of Book.txt file
+         */
+        public static final String SEPARATOR = ",";
+}
